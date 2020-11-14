5f5c65eeb86217c426d1cd3296bec2b2f2828940
kondig <digonisk@gmail.com>
2020-11-15 01:59:31 +0200

    [feat] add GA

--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 // import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
-// import createBrowserHistory from 'history/createBrowserHistory'
-// const history = createBrowserHistory()
+import { createBrowserHistory } from 'history';
 
 
+import ReactGA from 'react-ga';
+const trackingId = "UA-148338218-4";
+const history = createBrowserHistory();
+ReactGA.initialize(trackingId);
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 ReactDOM.render((
-  <HashRouter>
+  <Router history={history}>
     <App />
-  </HashRouter>
+  </Router>
 ), document.getElementById('root'));
 registerServiceWorker();
